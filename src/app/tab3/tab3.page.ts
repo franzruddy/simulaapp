bd090a5184f5272c745285717767057ee02b4b4a
salinasfranz <salinasfranzruddy@gmail.com>
2020-04-28 17:45:30 -0400

    insertar datos

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService, estructura } from '../services/firebase.service';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit { 
+  public calificacion: string;
+  public nombre: string;
+  public precio: string;
+  public tipo: string;
 
-  constructor() {}
+  constructor(
+    private firebase:FirebaseService
+  ) {}
 
+  ngOnInit(){
+  }
+  
+  agregar_videojuego(){
+    this.firebase.agregarPersona(this.calificacion,this.nombre,this.precio,this.tipo);
+    console.log('Video Juego Registrado');
+  }
 }
